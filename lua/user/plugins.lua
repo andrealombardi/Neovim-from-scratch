eafbc33b679d5a4241f1da8e3ce471d0dde3a84d
andrealombardi <andrea.lombardi@gmail.com>
2022-04-02 22:00:05 +0100

    Enable vim-go

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,6 +82,10 @@ return packer.startup(function(use)
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
+  -- programming
+  -- go
+  use "fatih/vim-go"
+
   -- Telescope
   use "nvim-telescope/telescope.nvim"
 
